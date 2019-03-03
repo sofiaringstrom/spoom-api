396651a8781cd0c4d0165b54f686a5235cf33739
sofiaringstrom <sofia.ringstrom@live.se>
2019-03-03 21:58:46 +0100

    Fixed api stuff

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-
-    
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import querystring from 'querystring';
+import cookieParser from 'cookie-parser';
 
 require('dotenv').config();
 
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
 const app = express();
 const PORT = process.env.PORT;
+const path = require('path');
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const SPOTIFY_REDIRECT_URL = process.env.SPOTIFY_REDIRECT_URL;
@@ -15,6 +34,10 @@ app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 });
 
+app.use(express.static(__dirname + '/public'))
+   .use(cors())
+   .use(cookieParser());
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -22,3 +45,38 @@ app.use((req, res, next) => {
   next()
 })
 app.use(bodyParser.json());
+
+app.get('/',function(req,res) {
+  console.log(req.query.code)
+
+  if (req.query.code) {
+    // code is present
+    res.sendFile(path.join(__dirname+'/auth.html'));
+  } else {
+    // code is not present, pls enter
+    res.sendFile(path.join(__dirname+'/enter-code.html'));
+  }
+
+  //res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private playlist-read-private';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: SPOTIFY_CLIENT_ID,
+      scope: scope,
+      redirect_uri: SPOTIFY_REDIRECT_URL,
+      state: state
+    }));
+});
+
+app.get('/done',function(req,res) {
+  res.sendFile(path.join(__dirname+'/done.html'));
+});
--- readme.md
+++ readme.md
@@ -17,3 +17,8 @@ npm run start
 
 ## Deploy
 Push to master, deploy on Heroku
+
+
+### Notes
+
+![note](https://i.imgur.com/UBLLkWy.png?1)
