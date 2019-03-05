352e103cb763c52cba6f5b13d58a3f57fa891e22
sofiaringstrom <sofia.ringstrom@live.se>
2019-03-05 23:37:50 +0100

    Fixed with tokens and authorization

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import querystring from 'querystring';
 import cookieParser from 'cookie-parser';
 
+let request = require('request')
 const io = require('socket.io')();
 
 require('dotenv').config();
@@ -34,6 +35,7 @@ const path = require('path');
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const SPOTIFY_REDIRECT_URL = process.env.SPOTIFY_REDIRECT_URL;
+const FRONTEND_URI = process.env.FRONTEND_URI;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
@@ -52,9 +54,28 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.get('/',function(req,res) {
-  console.log(req.query.code)
+  //console.log(req.query.code) code from app
 
-  if (req.query.code) {
+  if (req.query.access_token) {
+
+    // Authorized
+    console.log('authorized')
+
+    let authOptionsSecond = {
+      url: 'https://api.spotify.com/v1/me',
+      headers: {
+        'Authorization': 'Bearer ' + req.query.access_token
+      }
+    }
+
+    request.get(authOptionsSecond, (error, response) => {
+      console.log(response)
+      return res.status(200).send({
+        data: response
+      })
+    })
+
+  } else if (req.query.code) {
     // code is present
     res.sendFile(path.join(__dirname+'/auth.html'));
   } else {
@@ -62,7 +83,6 @@ app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/enter-code.html'));
   }
 
-  //res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.get('/login', function(req, res) {
@@ -71,11 +91,11 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private playlist-read-private';
+  var scope = 'user-read-private playlist-read-private user-read-email';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
-      response_type: 'code',
       client_id: SPOTIFY_CLIENT_ID,
+      response_type: 'code',
       scope: scope,
       redirect_uri: SPOTIFY_REDIRECT_URL,
       state: state
@@ -86,12 +106,106 @@ app.get('/done',function(req,res) {
   res.sendFile(path.join(__dirname+'/done.html'));
 });
 
-app.post('/callback', function(req, res) {
+app.get('/callback', function(req, res) {
   console.log('callback')
-  var code = req.body.code;
-  var access_token = req.body.access_token;
-  token_requests[code] = access_token;
-  console.log(token_requests)
+  
+  //var access_token = req.body.access_token;
+
+  console.log('callback', req.query)
+
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: SPOTIFY_REDIRECT_URL,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer.from(
+        SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    console.log('body', body)
+    var access_token = body.access_token;
+    var refresh_token = body.refresh_token;
+    let uri = process.env.FRONTEND_URI || 'http://localhost:7000'
+
+    // store tokens in app
+    // then send tokens to api when requesting data from spotify
+    var swotifyCode = req.cookies.swotify_code;
+    token_requests[swotifyCode] = {'access_token': access_token, 'refresh_token': refresh_token};
+
+    return res.status(200).send({
+      status: 'ok'
+    });
+
+    //res.redirect(uri + '?access_token=' + access_token + '&refresh_token=' + refresh_token);
+  })
+
+  /*request.post(authOptions, function(error, response, body) {
+    console.log(body)
+    var access_token = body.access_token;
+    var refresh_token = body.refresh_token;
+    console.log(access_token)
+
+    newToken = refresh_token;
+
+    token_requests[code] = {'access_token': access_token, 'refresh_token': refresh_token};
+    let uri = process.env.FRONTEND_URI || 'http://localhost:7000/callback';
+    res.redirect(uri + '?access_token=' + access_token)
+  })*/
+
+
+  /*let authOptionsSecond = {
+    url: 'https://api.spotify.com/v1/me',
+    headers: {
+      'Authorization': 'Bearer ' + newToken
+    }
+  }
+
+  console.log(newToken)
+
+  request.get(authOptionsSecond, (error, response) => {
+    console.log(response)
+  })*/
+
+  /*let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: access_token,
+      redirect_uri: SPOTIFY_REDIRECT_URL,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      Accept: 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (new Buffer.from(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }*/
+  /*request.post(authOptions, function(error, response, body) {
+    console.log(body)
+    var access_token = body.access_token;
+    var refresh_token = body.refresh_token;
+    console.log(access_token)
+
+    token_requests[code] = {'access_token': access_token, 'refresh_token': refresh_token};
+    /*let uri = process.env.FRONTEND_URI || 'http://localhost:7000'
+    res.redirect(uri + '?access_token=' + access_token)*/
+  //})
+
+  // från nytt anrop
+  /*token_requests[code] = access_token;
+  console.log(token_requests)*/
+
+  /*return res.status(200).send({
+    status: 'ok'
+  })*/
 });
 
 io.listen(SOCKET_PORT);
@@ -105,13 +219,15 @@ io.on('connection', (client) => {
     console.log('client is subscribing to code ', code);
 
     socketInterval = setInterval(() => {
+      console.log('socket connection')
+      console.log('token_requests', token_requests)
       if (token_requests[code]) {
 
         console.log('token valid')
         clearInterval(socketInterval);
-        var access_token = token_requests[code]
+        var authData = token_requests[code];
         delete token_requests[code]; 
-        client.emit('access_token', access_token);
+        client.emit('authData', authData);
 
       } else {
         console.log('no valid token found')
